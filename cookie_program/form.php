0519eeb8f56703462edb941ec938f11acd21f73d
Rushikesh <mrushikesh750@gmail.com>
2020-02-13 06:41:55 +0530

    13-02-2020

---
+++ cookie_program/form.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <form action="http://localhost/Rushi/cookie_program/backend.php" method="get">
+    email <input type="text" name="email" id="">
+    password <input type="password" name="password" id="">
+    <input type="submit" value="signup">
+    </form>
+
+
+</body>
+</html>
---
+++ cookie_program/login.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>title</title>
+    </head>
+    <body>
+<?php
+$email= $_COOKIE['email'];
+$pass= $_COOKIE['password'];
+echo "Logged in with $email and password $pass"
+?>
+    </body>
+</html>
